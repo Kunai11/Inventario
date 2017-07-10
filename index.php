35952535a6be084fc2e41b2576580b147a44d645
CarlosDavidVasquez <carlosdavidvasquez7@gmail.com>
2017-07-10 10:20:16 -0600

    Tabulacion y div contenedor centrado

--- index.php
+++ index.php
@@ -1,36 +1,37 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Menu Inventario</title>
-</head>
-</head>
-<body>
-<table position="center" border="1">
-	<tr>
-		<td>1</td>
-			<td><a href="ventas.php">Venta de Articulo</a></td>
-	</tr>
-	<tr>
-		<td>2</td>
-		<td><a href="agregar articulo.php">Agregar Descripcion de Nuevo Articulo</a></td>
-	</tr>
-	<tr>
-		<td>3</td>
-		<td><a href="modificar_articulo.php">Modificar Descripcion de Articulo Existente</a></td>
-	</tr>
-	<tr>
-		<td>4</td>
-		<td><a href="existencias.php">Agregar Existencias de Articulo</a></td>
-	</tr>
-	<tr>
-		<td>5</td>
-		<td><a href="consultar articulo.php">Consultar Lista de Articulos</a></td>
-	</tr>
-	<tr>
-		<td>6</td>
-		<td><a href="eliminar articulo.php">Elminar Articulo</a></td>
-	</tr>
-	
-</table>
-</body>
+	<head>
+		<title>Menu Inventario</title>
+	</head>
+
+	<body>
+		<div id="principal" margin="0 auto;">
+			<table position="center" border="1">
+				<tr>
+					<td>1</td>
+					<td><a href="ventas.php">Venta de Articulo</a></td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td><a href="agregar articulo.php">Agregar Descripcion de Nuevo Articulo</a></td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td><a href="modificar_articulo.php">Modificar Descripcion de Articulo Existente</a></td>
+				</tr>
+				<tr>
+					<td>4</td>
+					<td><a href="existencias.php">Agregar Existencias de Articulo</a></td>
+				</tr>
+				<tr>
+					<td>5</td>
+					<td><a href="consultar articulo.php">Consultar Lista de Articulos</a></td>
+				</tr>
+				<tr>
+					<td>6</td>
+					<td><a href="eliminar articulo.php">Elminar Articulo</a></td>
+				</tr>
+			</table>
+		</div>
+	</body>
 </html>
