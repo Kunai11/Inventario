d95d40841fb343a1c530647a8d23449ddede453b
CarlosDavidVasquez <carlosdavidvasquez7@gmail.com>
2017-07-09 23:05:55 -0600

    Vinculo al modulo de modificacion de articulo

--- index.php
+++ index.php
@@ -7,29 +7,28 @@
 <body>
 <table position="center" border="1">
 	<tr>
-		<td>1 </td>
+		<td>1</td>
 			<td><a href="ventas.php">Venta de Articulo</a></td>
 	</tr>
 	<tr>
-		<td>2 </td>
+		<td>2</td>
 		<td><a href="agregar articulo.php">Agregar Descripcion de Nuevo Articulo</a></td>
 	</tr>
-
-<tr>
+	<tr>
 		<td>3</td>
+		<td><a href="modificar_articulo.php">Modificar Descripcion de Articulo Existente</a></td>
+	</tr>
+	<tr>
+		<td>4</td>
 		<td><a href="existencias.php">Agregar Existencias de Articulo</a></td>
 	</tr>
-
 	<tr>
-		<td>4 </td>
-			<td><a href="consultar articulo.php">Consultar Lista de Articulos</a></td>
+		<td>5</td>
+		<td><a href="consultar articulo.php">Consultar Lista de Articulos</a></td>
 	</tr>
-
 	<tr>
-
-	<tr>
-		<td>5 </td>
-			<td><a href="eliminar articulo.php">Elminar Articulo</a></td>
+		<td>6</td>
+		<td><a href="eliminar articulo.php">Elminar Articulo</a></td>
 	</tr>
 	
 </table>
