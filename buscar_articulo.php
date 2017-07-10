3a1a0884a87fe323b34f8b0aea25c8c119bb5ec3
CarlosDavidVasquez <carlosdavidvasquez7@gmail.com>
2017-07-10 11:23:50 -0600

    Envio de la variable con el nombre de la pagina

---
+++ buscar_articulo.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Buscar producto</title>
+    </head>
+
+    <body>
+        <div id="principal">
+            <a href="index.php">Regresar</a>
+            <table border="1">
+                <th colspan="2" align="center"><h1>Buscar producto</h1></th>
+                <form method="GET" action="resultados.php">
+                <tr>
+                    <td>Buscar</td>
+                    <td>
+                        <input type="text" title="Parametros para la busqueda" name="palabra" id="palabra" required autofocus>
+                        <input type="submit" name="buscar" id="buscar" value="Buscar">
+                    </td>
+                </tr>
+                </form>
+            </table>
+        </div>
+    </body>
+</html>
