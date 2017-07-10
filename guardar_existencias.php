866d9b40da3568075a899b5ee83181f2e747f7c3
CarlosDavidVasquez <carlosdavidvasquez7@gmail.com>
2017-07-09 21:26:38 -0600

    Tabulacion

--- guardar_existencias.php
+++ guardar_existencias.php
@@ -1,10 +1,8 @@
 <?php
-include("cn/bdconexion.php");
-$codigo=$_POST['codigo_art'];
-$existencias=$_POST['existencias'];
+    include("cn/bdconexion.php");
+    $codigo=$_POST['codigo_art'];
+    $existencias=$_POST['existencias'];
 
-
-
-//echo $identidad.'-'.$nombre.'-'.$direccion.'-'.$telefono;
-$sql=mysqli_query($db,"INSERT INTO existencias(Cod_Articulo,Existencias) values ('$codigo','$existencias')")or die(mysqli_error());
+    //echo $identidad.'-'.$nombre.'-'.$direccion.'-'.$telefono;
+    $sql=mysqli_query($db,"INSERT INTO existencias(Cod_Articulo,Existencias) values ('$codigo','$existencias')")or die(mysqli_error());
 ?>
