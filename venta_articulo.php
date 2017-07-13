c3ec605742ac219b609fd6a761754c1037a1a5fa
CarlosDavidVasquez <carlosdavidvasquez7@gmail.com>
2017-07-12 23:31:52 -0600

    Tabulacion

--- venta_articulo.php
+++ venta_articulo.php
@@ -1,24 +1,23 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<title>Venta de articulo</title>
-</head>
-<body>
-	<a href="index.php">Regresar</a>
-	<table border="1">
-		 <th colspan="2" align="center"><h1>Venta Articulo</h1></th>
-		
-                   <form method="GET" action="buscar_articulo_venta.php"> 
-                    <tr> 
+    <head>
+        <meta charset="utf-8">
+        <title>Venta de articulo</title>
+    </head>
+    <body>
+        <a href="index.php">Regresar</a>
+        <table border="1">
+            <th colspan="2" align="center"><h1>Venta Articulo</h1></th>
+                <form method="GET" action="buscar_articulo_venta.php"> 
+                <tr> 
                     <td>
-                       <input type="submit" name="ver" id="ver" value="Ver Descripcion" >
+                        <input type="submit" name="ver" id="ver" value="Ver Descripcion" >
                     </td>
                     <td>
-                    	<input type="number" name="palabra" id="palabra">
+                        <input type="number" name="palabra" id="palabra">
                     </td>
                 </tr>
-	</table>
-</body>
+        </table>
+    </body>
 </html>
 
