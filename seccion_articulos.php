5d0d9ff7a439c39fb9504910a2ba0a2959a999f2
CarlosDavidVasquez <carlosdavidvasquez7@gmail.com>
2017-07-13 10:39:18 -0600

    Estructura visual de la seccion Articulos

---
+++ seccion_articulos.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es" >
+	<head>
+		<meta charset="utf-8">
+		<title>Sección Artículos | Control Comercial</title>
+		<link rel="shortcut icon" href="img/logo.png">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+
+		<header>
+			
+			<div class="encabezado" id="top">
+				<img src="img/logo.png" class="img-responsive">
+				<h1><a href="index.php"><strong>Control Comercial</strong></a></h1>
+			</div>
+
+		</header>
+
+		<div id="nav">
+				<a href="index.php">Inicio</a>
+				<a href="seccion_articulos">Art&iacute;culos</a>
+				<a href="#">Transacciones</a>
+				<a href="#">Cat&aacute;logo</a>
+				<a href="#">Historial</a>
+		</div>
+
+		<div id="contenedor">
+			<div class="contenido" id="izq">
+				<h2>Sección Artículos</h2>
+				<hr>
+				<h3>¿Que acción desea realizar?</h3>
+				<ul>
+					<li><a href="agregar_articulo.php">Agregar nuevo articulo</a></li>
+					<li><a href="modificar_articulo.php">Modificar articulo</a></li>
+					<li><a href="eliminar_articulo.php">Eliminar articulo</a></li>
+				</ul>
+			</div>
+
+			<div class="contenido" id="der">
+				<h2>Información</h2>
+				<hr>
+				<p>Por favor, seleccione la acción que desea realizar en el listado que se muestra, o puede ir a otra sección en la barra de navegación superior.</p>
+			</div>
+		</div>
+		<div id="footer">
+				<h6>&copy; Copyright 2017 | Carlos Meza | Carlos Vásquez  </h6>
+			</div>
+	</body>
+</html>
