ec71a531c9fb86f4543931d931387b2db60aa67f
CarlosDavidVasquez <carlosdavidvasquez7@gmail.com>
2017-07-12 23:19:43 -0600

    Verificar si el codigo ingresado ya existe, si es asi mostrar un mensaje de error sino permitir que se guarde el nuevo registro

--- guardar_nuevo_producto.php
+++ guardar_nuevo_producto.php
@@ -7,13 +7,22 @@
     $categoria=$_POST['categoria'];
 
     //echo $identidad.'-'.$nombre.'-'.$direccion.'-'.$telefono;
-    $sql=mysqli_query($db,"INSERT INTO catalogo(Codigo,Descripcion,Costo,Precio,Categoria) values ('$codigo','$descripcion','$costo','$precio','$categoria')")or die(mysqli_error());
 
-      $sql=mysqli_query($db,"INSERT INTO existencias(Codigo_Art) values ('$codigo')")or die(mysqli_error());
+    $sql=mysqli_query($db,"SELECT Codigo FROM catalogo WHERE Codigo = '$codigo' ")or die(mysqli_error());
 
+    if (mysqli_fetch_array($sql)) {
+        echo '
+        <p>Error el art&iacute;culo no se puede agregar.</p>
+        <p>Ya existe el Codigo ingresado.</p>
+        <p><a href="agregar_articulo.php">Regresar</a></p>
+        ';
+    }else{
+        $sql=mysqli_query($db,"INSERT INTO catalogo(Codigo,Descripcion,Costo,Precio,Categoria) values ('$codigo','$descripcion','$costo','$precio','$categoria')")or die(mysqli_error());
+        $sql=mysqli_query($db,"INSERT INTO existencias(Codigo_Art) values ('$codigo')")or die(mysqli_error());
 
-    echo '
+        echo '
         <p>Art&iacute;culo agregado exitosamente.</p>
         <p><a href="index.php">Regresar</a></p>
     ';
+    }
 ?>
