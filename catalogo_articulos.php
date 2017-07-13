e851020716037ca2203e00447a708c37d18a6e46
CarlosDavidVasquez <carlosdavidvasquez7@gmail.com>
2017-07-13 14:58:29 -0600

    Diseño para el modulo de compras

--- catalogo_articulos.php
+++ catalogo_articulos.php
@@ -1,41 +1,77 @@
 <!DOCTYPE html>
-<html>
-    <head>
-    	<meta charset="utf-8">
-    	<title>Catalogo</title>
-    </head>
-    <body>
-        <div id="principal">
-        <a href="index.php">Regresar</a>
-        <table border="1">
-            <tr>
-                <td colspan="6" align="center"><h1>Catalogo</h1></td>
-            </tr>
-            <tr>
-                <th>Codigo</th>
-                <th>Descripci&oacute;n</th>
-                <th>Costo</th>
-                <th>Precio</th>
-                <th>Cat&aacute;logo</th>
-                <th>Existencias</th>
-            </tr>
-            <?php
-                include("cn/bdconexion.php");
-                $sql=mysqli_query($db,"SELECT C.*, E.Existencias FROM catalogo C INNER JOIN existencias E ON C.Codigo = E.Codigo_Art ") or die(mysqli_error());
-                while($row=mysqli_fetch_array($sql)) {
-                    echo '
-                    <tr>
-                        <td>'.$row['Codigo'].'</td>
-                        <td>'.$row['Descripcion'].'</td>
-                        <td>'.$row['Costo'].'</td>
-                        <td>'.$row['Precio'].'</td>
-                        <td>'.$row['Categoria'].'</td>
-                        <td>'.$row['Existencias'].'</td>
-                    </tr>
-                    ';
-                }
-            ?>
-        </table>
-    </div>
-</body>
+<html lang="es" >
+	<head>
+		<meta charset="utf-8">
+		<title>Catálogo de Artículos | Control Comercial</title>
+		<link rel="shortcut icon" href="img/logo.png">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+
+		<header>
+			
+			<div class="encabezado" id="top">
+				<img src="img/logo.png" class="img-responsive">
+				<h1><a href="index.php"><strong>Control Comercial</strong></a></h1>
+			</div>
+
+		</header>
+
+		<div id="nav">
+				<a href="index.php">Inicio</a>
+				<a href="seccion_articulos.php">Art&iacute;culos</a>
+				<a href="seccion_transacciones.php">Transacciones</a>
+				<a href="seccion_catalogo.php">Cat&aacute;logo</a>
+				<a href="seccion_historial.php">Historiales</a>
+		</div>
+
+		<div id="contenedor">
+			<div class="contenido" id="izq">
+				<h2>Catálogo de artículo</h2>
+				<hr>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Codigo</th>
+                            <th>Descripcion</th>
+                            <th>Costo</th>
+                            <th>Precio</th>
+                            <th>Categoria</th>
+                            <th>Existencias</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            include("cn/bdconexion.php");
+                            $sql=mysqli_query($db,"SELECT C.*, E.Existencias FROM catalogo C INNER JOIN existencias E ON C.Codigo = E.Codigo_Art ") or die(mysqli_error());
+                            while($row=mysqli_fetch_array($sql)) {
+                                echo '
+                                <tr>
+                                    <td>'.$row['Codigo'].'</td>
+                                    <td>'.$row['Descripcion'].'</td>
+                                    <td>'.$row['Costo'].'</td>
+                                    <td>'.$row['Precio'].'</td>
+                                    <td>'.$row['Categoria'].'</td>
+                                    <td>'.$row['Existencias'].'</td>
+                                </tr>
+                                ';
+                            }
+                        ?>
+                    </tbody>
+                </table>
+                <hr>
+                <p><a href="index.php">Regresar</a></p>
+			</div>
+
+			<div class="contenido" id="der">
+				<h2>Información</h2>
+				<hr>
+                <p>Aquí se muestran los detalles de todos los artículos existentes en la base de datos, también sus respectivos valores en existencias.</p>
+                <p>Si no logra identificar un artículo en específico, puede navegar al módulo "Buscar un artículo en particular" en la sección de "Catálogo", o puede hacer click <a href="buscar_articulo.php">aquí</a>.</p>
+			</div>
+		</div>
+		<div id="footer">
+				<h6>&copy; Copyright 2017 | Carlos Meza | Carlos Vásquez  </h6>
+			</div>
+	</body>
 </html>
--- comprar_articulo.php
+++ comprar_articulo.php
@@ -1,4 +1,85 @@
 <!DOCTYPE html>
+<html lang="es" >
+	<head>
+		<meta charset="utf-8">
+		<title>Comprar Artículo | Control Comercial</title>
+		<link rel="shortcut icon" href="img/logo.png">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+
+		<header>
+			
+			<div class="encabezado" id="top">
+				<img src="img/logo.png" class="img-responsive">
+				<h1><a href="index.php"><strong>Control Comercial</strong></a></h1>
+			</div>
+
+		</header>
+
+		<div id="nav">
+				<a href="index.php">Inicio</a>
+				<a href="seccion_articulos.php">Art&iacute;culos</a>
+				<a href="seccion_transacciones.php">Transacciones</a>
+				<a href="seccion_catalogo.php">Cat&aacute;logo</a>
+				<a href="seccion_historial.php">Historiales</a>
+		</div>
+
+		<div id="contenedor">
+			<div class="contenido" id="izq">
+				<h2>Comprar artículo</h2>
+				<hr>
+
+				<form method="POST" action="guardar_compra.php">
+					<p>
+						Art&iacute;culo<br>
+						<div class="select">
+							<select name="codigo_art" name="codigo_art" required tabindex="1">
+								<?php
+									include("cn/bdconexion.php");
+									$sql=mysqli_query($db,"SELECT * FROM catalogo") or die(mysqli_error());
+									while($row=mysqli_fetch_array($sql)) {
+										echo '
+											<option value="'.$row['Codigo'].'">'.$row['Descripcion'].'</option>
+										';
+									}
+								?>
+							</select>
+						</div>
+					</p>
+					<p>
+						Cantidad de unidades<br>
+						<input type="number" min="1" name="cantidad" id="cantidad" required tabindex="2">
+					</p>							
+					<p>
+						Fecha de transacción<br>
+						<input type="date" name="fecha" id="fecha" required tabindex="3">
+					</p>
+					<hr>
+					<p>
+						<input class="btn" type="reset" name="limpiar" tabindex="4">
+						<input class="btn" type="submit" name="guargar" value="Guardar" tabindex="5">
+					</p>
+				</form>
+
+			</div>
+
+			<div class="contenido" id="der">
+				<h2>Información</h2>
+				<hr>
+				<p>Por favor, rellene todos los campos con la información correspondiente y haga click en el botón guardar cuando haya finalizado.</p>
+				<p>Para borrar todos los campos, haga click en el botón Limpiar.</p>
+			</div>
+		</div>
+		<div id="footer">
+				<h6>&copy; Copyright 2017 | Carlos Meza | Carlos Vásquez  </h6>
+			</div>
+	</body>
+</html>
+
+<!--
+
+<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
@@ -19,17 +100,16 @@
 					<td>
 						<select name="codigo_art" name="codigo_art" required tabindex="1">
 							<?php
-								include("cn/bdconexion.php");
-								$sql=mysqli_query($db,"SELECT * FROM catalogo") or die(mysqli_error());
-								while($row=mysqli_fetch_array($sql)) {
-									echo '
-										<option value="'.$row['Codigo'].'">'.$row['Descripcion'].'</option>
-									';
-								}
+								// include("cn/bdconexion.php");
+								// $sql=mysqli_query($db,"SELECT * FROM catalogo") or die(mysqli_error());
+								// while($row=mysqli_fetch_array($sql)) {
+								// 	echo '
+								// 		<option value="'.$row['Codigo'].'">'.$row['Descripcion'].'</option>
+								// 	';
+								// }
 							?>
 						</select>
 					</td>
-					<!-- <td><input type="number" name="codigo_art" id="codigo_art"></td> -->
 				</tr>
 				<tr>
 					<td>Cantidad de unidades</td>
@@ -49,3 +129,4 @@
 		</form>
 	</body>
 </html>
+-->
