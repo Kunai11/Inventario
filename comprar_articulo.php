e71b0678582270b62c42e6a17f2e60bdbf267002
CarlosDavidVasquez <carlosdavidvasquez7@gmail.com>
2017-07-12 22:36:20 -0600

    Cambiar el ingreso del codigo del articulo por la seleccion de uno de la lista en un select

--- comprar_articulo.php
+++ comprar_articulo.php
@@ -5,6 +5,9 @@
 		<title>Comprar articulo</title>
 	</head>
 	<body>
+		<?php
+			
+		?>
 		<a href="index.php">Regresar</a>
 		<form method="POST" action="guardar_compra.php">
 			<table position="center" border="1">
@@ -12,21 +15,34 @@
 					<td colspan="3" align="center"><h1>Compra</h1></td>
 				</tr>
 				<tr>
-					<td>Codigo de articulo</td>
-					<td><input type="number" name="codigo_art" id="codigo_art"></td>
+					<td>Art&iacute;culo</td>
+					<td>
+						<select name="codigo_art" name="codigo_art" required tabindex="1">
+							<?php
+								include("cn/bdconexion.php");
+								$sql=mysqli_query($db,"SELECT * FROM catalogo") or die(mysqli_error());
+								while($row=mysqli_fetch_array($sql)) {
+									echo '
+										<option value="'.$row['Codigo'].'">'.$row['Descripcion'].'</option>
+									';
+								}
+							?>
+						</select>
+					</td>
+					<!-- <td><input type="number" name="codigo_art" id="codigo_art"></td> -->
 				</tr>
 				<tr>
 					<td>Cantidad de unidades</td>
-					<td><input type="number" name="cantidad" id="cantidad"></td>
+					<td><input type="number" name="cantidad" id="cantidad" required tabindex="2"></td>
 				</tr>
 				<tr>
 					<td>Fecha de transaccion</td>
-					<td><input type="date" name="fecha" id="fecha"></td>
+					<td><input type="date" name="fecha" id="fecha" required tabindex="3"></td>
 				</tr>
 				<tr>
 					<td colspan="2" align="center">
-						<input type="reset" name="limpiar">
-						<input type="submit" name="guargar" value="Guardar">
+						<input type="reset" name="limpiar" tabindex="4">
+						<input type="submit" name="guargar" value="Guardar" tabindex="5">
 					</td>
 				</tr>
 			</table>
