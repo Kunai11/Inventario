6789940a1f365f3c129f6ed8b2165a4ffc87a94b
CarlosDavidVasquez <carlosdavidvasquez7@gmail.com>
2017-07-13 11:25:04 -0600

    Disenio Modulo Agregar

--- agregar_articulo.php
+++ agregar_articulo.php
@@ -1,43 +1,74 @@
 <!DOCTYPE html>
-<html>
+<html lang="es" >
 	<head>
 		<meta charset="utf-8">
-		<title>Agregar nuevo producto</title>
+		<title>Agregar Artículo | Control Comercial</title>
+		<link rel="shortcut icon" href="img/logo.png">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
 	<body>
-		<a href="index.php">Regresar</a>
-		<form method="POST" action="guardar_nuevo_producto.php">
-			<table border="1">
-				<tr>
-					<td colspan="2" align="center"><h1>Agregar nuevo producto</h1></td>
-				</tr>
-				<tr>
-					<td>Codigo:</td>
-					<td><input  type="number"  name="codigo_art" id="codigo_art" required autofocus tabindex="1"></td>
-				</tr>
-				<tr>
-					<td>Descripci&oacute;n:</td>
-					<td><input type="text" name="descripcion" id="descripcion" required tabindex="2"></td>
-				</tr>
-				<tr>
-					<td>Costo:</td>
-					<td><input type="number" name="costo" id="costo" required tabindex="3" ></td>
-				</tr>
-				<tr>
-					<td>Precio:</td>
-					<td><input type="number" name="precio" id="precio" required tabindex="4" ></td>
-				</tr>
-				<tr>
-					<td>Categoria:</td>
-					<td><input type="text" name="categoria" id="categoria" required tabindex="5"></td>
-				</tr>
-				<tr>
-					<td colspan="2" align="center">
-						<input type="reset" name="limpiar">
-						<input type="submit" name="guargar" value="Guardar">
-					</td>
-				</tr>
-			</table>
-		</form>
+
+		<header>
+			
+			<div class="encabezado" id="top">
+				<img src="img/logo.png" class="img-responsive">
+				<h1><a href="index.php"><strong>Control Comercial</strong></a></h1>
+			</div>
+
+		</header>
+
+		<div id="nav">
+				<a href="index.php">Inicio</a>
+				<a href="seccion_articulos.php">Art&iacute;culos</a>
+				<a href="seccion_transacciones.php">Transacciones</a>
+				<a href="seccion_catalogo.php">Cat&aacute;logo</a>
+				<a href="seccion_historial.php">Historiales</a>
+		</div>
+
+		<div id="contenedor">
+			<div class="contenido" id="izq">
+				<h2>Agregar artículo</h2>
+				<hr>
+
+				<form method="POST" action="guardar_nuevo_producto.php">
+					<p>
+						Codigo:<br>
+						<input  type="number" min="1" name="codigo_art" id="codigo_art" required autofocus tabindex="1">
+					</p>
+					<p>
+						Descripci&oacute;n:<br>
+						<input type="text" name="descripcion" id="descripcion" required tabindex="2">
+					</p>							
+					<p>
+						Costo:<br>
+						<input type="number" min="1" name="costo" id="costo" required tabindex="3" >
+					</p>
+					<p>
+						Precio:<br>
+						<input type="number" min="1" name="precio" id="precio" required tabindex="4" >
+					</p>
+					<p>
+						Categoria:<br>
+						<input type="text" name="categoria" id="categoria" required tabindex="5">
+					</p>
+					<hr>
+					<p>
+						<input class="btn" type="reset" name="limpiar" value="Limpiar">
+						<input class="btn" type="submit" name="guargar" value="Guardar">
+					</p>
+				</form>
+
+			</div>
+
+			<div class="contenido" id="der">
+				<h2>Información</h2>
+				<hr>
+				<p>Por favor, rellene todos los campos con la información correspondiente y haga click en el botón guardar cuando haya finalizado.</p>
+				<p>Para borrar todos los campos, haga click en el botón Limpiar.</p>
+			</div>
+		</div>
+		<div id="footer">
+				<h6>&copy; Copyright 2017 | Carlos Meza | Carlos Vásquez  </h6>
+			</div>
 	</body>
 </html>
--- guardar_nuevo_producto.php
+++ guardar_nuevo_producto.php
@@ -1,28 +1,73 @@
-<?php
-    include("cn/bdconexion.php");
-    $codigo=$_POST['codigo_art'];
-    $descripcion=$_POST['descripcion'];
-    $costo=$_POST['costo'];
-    $precio=$_POST['precio'];
-    $categoria=$_POST['categoria'];
+<!DOCTYPE html>
+<html lang="es" >
+	<head>
+		<meta charset="utf-8">
+		<title>Guardando Nuevo Artículo | Control Comercial</title>
+		<link rel="shortcut icon" href="img/logo.png">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
 
-    //echo $identidad.'-'.$nombre.'-'.$direccion.'-'.$telefono;
+		<header>
+			
+			<div class="encabezado" id="top">
+				<img src="img/logo.png" class="img-responsive">
+				<h1><a href="index.php"><strong>Control Comercial</strong></a></h1>
+			</div>
 
-    $sql=mysqli_query($db,"SELECT Codigo FROM catalogo WHERE Codigo = '$codigo' ")or die(mysqli_error());
+		</header>
 
-    if (mysqli_fetch_array($sql)) {
-        echo '
-        <p>Error el art&iacute;culo no se puede agregar.</p>
-        <p>Ya existe el Codigo ingresado.</p>
-        <p><a href="agregar_articulo.php">Regresar</a></p>
-        ';
-    }else{
-        $sql=mysqli_query($db,"INSERT INTO catalogo(Codigo,Descripcion,Costo,Precio,Categoria) values ('$codigo','$descripcion','$costo','$precio','$categoria')")or die(mysqli_error());
-        $sql=mysqli_query($db,"INSERT INTO existencias(Codigo_Art) values ('$codigo')")or die(mysqli_error());
+		<div id="nav">
+				<a href="index.php">Inicio</a>
+				<a href="seccion_articulos.php">Art&iacute;culos</a>
+				<a href="seccion_transacciones.php">Transacciones</a>
+				<a href="seccion_catalogo.php">Cat&aacute;logo</a>
+				<a href="seccion_historial.php">Historiales</a>
+		</div>
 
-        echo '
-        <p>Art&iacute;culo agregado exitosamente.</p>
-        <p><a href="index.php">Regresar</a></p>
-    ';
-    }
-?>
+		<div id="contenedor">
+			<div class="contenido" id="izq">
+				<h2>Guardando nuevo artículo</h2>
+				<hr>
+                <?php
+                    include("cn/bdconexion.php");
+                    $codigo=$_POST['codigo_art'];
+                    $descripcion=$_POST['descripcion'];
+                    $costo=$_POST['costo'];
+                    $precio=$_POST['precio'];
+                    $categoria=$_POST['categoria'];
+
+                    //echo $identidad.'-'.$nombre.'-'.$direccion.'-'.$telefono;
+
+                    $sql=mysqli_query($db,"SELECT Codigo FROM catalogo WHERE Codigo = '$codigo' ")or die(mysqli_error());
+
+                    if (mysqli_fetch_array($sql)) {
+                        echo '
+                        <p color="red">Error el art&iacute;culo no se puede agregar.</p>
+                        <p>Ya existe el Codigo ingresado.</p>
+                        <p><a href="agregar_articulo.php">Regresar</a></p>
+                        ';
+                    }else{
+                        $sql=mysqli_query($db,"INSERT INTO catalogo(Codigo,Descripcion,Costo,Precio,Categoria) values ('$codigo','$descripcion','$costo','$precio','$categoria')")or die(mysqli_error());
+                        $sql=mysqli_query($db,"INSERT INTO existencias(Codigo_Art) values ('$codigo')")or die(mysqli_error());
+
+                        echo '
+                        <p>Art&iacute;culo guardado exitosamente.</p>
+                        <p><a href="index.php">Volver al Inicio</a></p>
+                    ';
+                    }
+                ?>
+			</div>
+
+			<div class="contenido" id="der">
+				<h2>Información</h2>
+				<hr>
+				<p>Por favor, rellene todos los campos con la información correspondiente y haga click en el botón guardar cuando haya finalizado.</p>
+				<p>Para borrar todos los campos, haga click en el botón Limpiar.</p>
+			</div>
+		</div>
+		<div id="footer">
+				<h6>&copy; Copyright 2017 | Carlos Meza | Carlos Vásquez  </h6>
+			</div>
+	</body>
+</html>
