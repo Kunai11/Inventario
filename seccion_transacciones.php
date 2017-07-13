8d0bee7dbcda034d99c74a814043bdbc48e7c174
CarlosDavidVasquez <carlosdavidvasquez7@gmail.com>
2017-07-13 10:54:48 -0600

    Seccion Transacciones

---
+++ seccion_transacciones.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es" >
+	<head>
+		<meta charset="utf-8">
+		<title>Sección Transacciones | Control Comercial</title>
+		<link rel="shortcut icon" href="img/logo.png">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+
+		<header>
+			
+			<div class="encabezado" id="top">
+				<img src="img/logo.png">
+				<h1><a href="index.php"><strong>Control Comercial</strong></a></h1>
+			</div>
+
+		</header>
+
+		<div id="nav">
+				<a href="index.php">Inicio</a>
+				<a href="seccion_articulos.php">Art&iacute;culos</a>
+				<a href="seccion_transacciones.php">Transacciones</a>
+				<a href="seccion_catalogo.php">Cat&aacute;logo</a>
+				<a href="seccion_histrorial.php">Historial</a>
+		</div>
+
+		<div id="contenedor">
+			<div class="contenido" id="izq">
+				<h2>Sección Transacciones</h2>
+				<hr>
+				<h3>¿Que transacción desea realizar?</h3>
+				<ul>
+					<li><a href="comprar_articulo.php">Comprar articulos</a></li>
+					<li><a href="venta_articulo.php">Vender articulos</a></li>
+				</ul>
+			</div>
+
+			<div class="contenido" id="der">
+				<h2>Información</h2>
+				<hr>
+				<p>Por favor, seleccione la transacción que desea realizar en el listado que se muestra, o puede ir a otra sección en la barra de navegación superior.</p>
+			</div>
+		</div>
+		<div id="footer">
+				<h6>&copy; Copyright 2017 | Carlos Meza | Carlos Vásquez  </h6>
+			</div>
+	</body>
+</html>
